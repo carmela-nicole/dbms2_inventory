573aec71e17b49d46b382fb5fc7ef6ed62a42e55
carmela-nicole <bautistacarmelanicole@gmail.com>
2023-11-02 22:27:33 +0800

    Initial commit

---
+++ database/migrations/2023_09_30_021919_create_inventory_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('student_name');
+            $table->string('items');
+            $table->string('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory');
+    }
+};
